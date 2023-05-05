2e2f7d0b73581830824af7e1d6e7d5b8acf38729
ergashev7 <dior7354423@gmail.com>
2023-05-06 04:07:50 +0500

    array basic

---
+++ array.js
@@ -0,0 +1,31 @@
+// Array
+// let chevrolet1 = "gentra";
+// let chevrolet2 = "cobalt";
+// let chevrolet3 = "malibu";
+// let chevrolet4 = "onix";
+// console.log(chevrolet1.toUpperCase());
+// console.log(chevrolet2.toUpperCase());
+// console.log(chevrolet3.toUpperCase());
+// console.log(chevrolet4.toUpperCase());
+// console.log(array.map((element) => element.toUpperCase()));
+// console.log(array[3]);
+// console.log(array[array.length - 1]);
+// console.log(array.at(-1));
+// for (i = 0; i < array.length; i++) {
+//   //   console.log(array[i]);
+// }
+// for (let ar of array) {
+//   console.log(ar);
+// }
+// for (let a in array) {
+//     console.log(array[a]);
+// }
+// console.log(array.indexOf("onix"));
+// console.log(array.lastIndexOf("onix"));
+// let array = ["gentra", "cobalt", "malibu", "onix", "damas", "onix"];
+// console.log(ar.slice(2, 4));
+// console.log(ar.splice(2, 4));
+// let str = "Diyorbek";
+// let ar = [0, 1, 2, 3, 4];
+// console.log(str.split());
+// console.log(ar.reverse());
